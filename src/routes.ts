b6fbe28e485b1c2252f12abe41a817b599a8a952
Joshua Sherman <joshua.v.sherman@gmail.com>
2021-02-17 08:06:27 -0500

    update code climate config

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express';
 import user from './model/user/user-router';
-import book from './model/blog/blog-router';
+import blog from './model/blog/blog-router';
 import email from './model/email/email-router';
 
 const router = express.Router();
@@ -8,6 +8,6 @@ const router = express.Router();
 export default function route(app: Express, baseUrl:string): void {
   app.use(router);
   router.use(`${baseUrl}/user`, user);
-  router.use(`${baseUrl}/book`, book);
+  router.use(`${baseUrl}/blog`, blog);
   router.use(`${baseUrl}/email`, email);
 }
